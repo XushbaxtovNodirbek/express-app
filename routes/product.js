0fcc9adb9c35d662e8c1ed702680e46a8a6abb77
nodirbek <nodirbek.devoloper@gmail.com>
2023-11-29 17:31:19 +0500

    CRUD completed

--- routes/product.js
+++ routes/product.js
@@ -1,22 +1,25 @@
 import { Router } from "express";
 import Product from "../models/Product.js";
 import {getUserId,checkTokenIsEx} from "../services/token.js";
-import User from "../models/User.js";
 
 const router = Router();
 
-router.get('/',(req,res) => {
+router.get('/',async(req,res) => {
+    // RENDER HOME PAGE
     res.render('index',{
-        title:'Shop | Home'
+        title:'Shop | Home',
+        products:await Product.find().lean()
     })
 });
 
 router.get('/add',(req,res) => {
+    // CHECK IF TOKEN IS EXPIRED
     if(checkTokenIsEx(req.cookies.token)){
         res.clearCookie('token');
         res.redirect('/login')
         return
     }
+    // RENDER ADD PAGE
     res.render('add',{
         title:'Product | Add',
         isAdd: true,
@@ -26,12 +29,23 @@ router.get('/add',(req,res) => {
 
 router.post('/add-products',async (req,res) =>{
     // GET DATA FROM FORM
-    const { title, description, image, price} = req.body;
+    const { title, description, image, price, productId} = req.body;
     // CHECK IF ALL FIELDS ARE FILLED
     if(!title || !description || !image || !price){
         req.flash('prductsaveError','All fields are required');
         res.redirect('/add');
         return
+    }
+    // CHECK IF PRODUCT EXIST
+    if(productId){
+       await Product.findByIdAndUpdate(productId,{
+        title,
+        description,
+        image,
+        price
+       })
+         res.redirect('/products');
+       return
     }
     // GET USER ID FROM TOKEN
     const userId = getUserId(req.cookies.token);
@@ -48,16 +62,53 @@ router.post('/add-products',async (req,res) =>{
     
 })
 
-router.get('/products',(req,res) => {
+router.get('/products',async(req,res) => {
+    // CHECK IF TOKEN IS EXPIRED
     if(checkTokenIsEx(req.cookies.token)){
         res.clearCookie('token');
         res.redirect('/login')
         return
     }
+    // GET PRODUCTS FROM DB
+    const products = await Product.find({userId: getUserId(req.cookies.token)}).lean();
+    // RENDER PRODUCTS PAGE
     res.render('product',{
         title:'Shop | Products',
-        isProduct: true
+        isProduct: true,
+        products: products
     })
 });
+// DETAIL PAGE
+router.get('/product-about',async (req,res) => {
+    // GET ID FROM URL
+    const id = req.query.id;
+    // GET PRODUCT FROM DB
+    const product = await Product.findById(id).lean();
+    // RENDER PRODUCT PAGE
+    res.render('detail',{
+        title:'Shop | Product',
+        isProduct: true,
+        product: product
+    })
+})
+// DELETE PRODUCT
+router.get('/product-delete',async (req,res) => {
+    // GET ID FROM URL
+    const id = req.query.id;
+    // DELETE PRODUCT FROM DB
+    await Product.findByIdAndDelete(id);
+    // REDIRECT TO PRODUCTS PAGE
+    res.redirect('/products');
+})
+// EDIT PRODUCT
+router.get('/product-edit',async (req,res) => {
+    // GET ID FROM URL
+    const productId = req.query.id;
+    // RENDER EDIT PAGE
+    res.render('add',{
+      title:'Shop | Edit',
+      productId  
+    })
+})
 
 export default router;
