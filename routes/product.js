805692681eecdc9420956a281eadc4ec7cddafe1
nodirbek <nodirbek.devoloper@gmail.com>
2023-11-29 15:36:18 +0500

    create product model and save to db,and check token function added

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import Product from "../models/Product.js";
+import {getUserId,checkTokenIsEx} from "../services/token.js";
+import User from "../models/User.js";
 
 const router = Router();
 
@@ -9,13 +12,48 @@ router.get('/',(req,res) => {
 });
 
 router.get('/add',(req,res) => {
+    if(checkTokenIsEx(req.cookies.token)){
+        res.clearCookie('token');
+        res.redirect('/login')
+        return
+    }
     res.render('add',{
         title:'Product | Add',
-        isAdd: true
+        isAdd: true,
+        prductsaveError: req.flash('prductsaveError')
     })
 });
 
+router.post('/add-products',async (req,res) =>{
+    // GET DATA FROM FORM
+    const { title, description, image, price} = req.body;
+    // CHECK IF ALL FIELDS ARE FILLED
+    if(!title || !description || !image || !price){
+        req.flash('prductsaveError','All fields are required');
+        res.redirect('/add');
+        return
+    }
+    // GET USER ID FROM TOKEN
+    const userId = getUserId(req.cookies.token);
+    // CREATE NEW PRODUCT
+    Product.create({
+        title,
+        description,
+        image,
+        price,
+        userId
+    }).then((product) => {console.log(product);})
+    // REDIRECT TO PRODUCTS PAGE
+    res.redirect('/products');
+    
+})
+
 router.get('/products',(req,res) => {
+    if(checkTokenIsEx(req.cookies.token)){
+        res.clearCookie('token');
+        res.redirect('/login')
+        return
+    }
     res.render('product',{
         title:'Shop | Products',
         isProduct: true
--- services/token.js
+++ services/token.js
@@ -1,8 +1,22 @@
 import jwt from 'jsonwebtoken';
-
+// GENERATE TOKEN
 const generateToken = (userId) => {
-    const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
     return accessToken;
 }
+// GET USER ID FROM TOKEN
+const getUserId = (token) => {
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    return data.userId;
+}
+// CHECK IF TOKEN IS EXPIRED
+const checkTokenIsEx = (token) => {
+    try {
+        jwt.verify(token, process.env.JWT_SECRET);
+        return false
+    } catch (error) {
+        return true
+    }
+}
 
-export { generateToken}
+export { generateToken,getUserId ,checkTokenIsEx}
